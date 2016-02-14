f274fcb1c9a87f727449d17e47dc9c0cc14bbbd9
Orion <orionmcc@gmail.com>
2016-02-14 16:48:16 -0500

    Added editor build script. Added Var editing for editor.

---
+++ scripts/buildeditor.js
@@ -0,0 +1,46 @@
+var fs = require('fs');
+var UglifyJS = require('uglify-js');
+
+function concat(opts) {
+    var fileList = opts.src;
+    var distPath = opts.dest;
+    var out = fileList.map(function(filePath){
+            return fs.readFileSync(__dirname+"/"+filePath).toString();
+        });
+    fs.writeFileSync(__dirname+"/"+distPath, out.join('\n'));
+    console.log(' '+ distPath +' built.');
+}
+
+concat({
+    src : [
+        '../lib/editor_src/editor.js',
+        '../lib/editor_src/select.js',
+        '../lib/editor_src/data.js',
+        '../lib/editor_src/page.js',
+        '../lib/editor_src/tinymce.js',
+    ],
+    dest : '../lib/porosedit.js'
+});
+
+
+function uglify(srcPath, distPath) {
+  var result = UglifyJS.minify(__dirname+"/"+srcPath, {
+    mangle: true,
+    compress: {
+      sequences: true,
+      dead_code: true,
+      conditionals: true,
+      booleans: true,
+      unused: true,
+      if_return: true,
+      join_vars: true,
+      drop_console: true
+    }
+  });
+
+  fs.writeFileSync(__dirname+"/"+distPath, result.code);
+
+    console.log(' '+ distPath +' built.');
+}
+
+uglify('../lib/porosedit.js', '../lib/porosedit.min.js');
