d01c08f5cbbc0faaef0f4df0f175e8b26e6d1891
orionmcc <orionmcc@gmail.com>
2020-04-29 02:11:50 -0400

    Switch from mailgun to sendgrid

---
+++ scripts/testemail.mjs
@@ -0,0 +1,8 @@
+
+'use strict';
+
+import EmailAdapter from '../lib/email';
+import { server as serverSettings } from "../local";
+
+let EMAIL = new EmailAdapter();
+EMAIL.send("info@unpossiblegamelabs.com", { from: 'info@unpossibleworks.com', template: 'd-91855398cd1d4310bd5f4582b9b6d062', variables: { Server: serverSettings.host } });
