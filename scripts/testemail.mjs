b4241fb4b0e4c4e936683624173b06ea4ead4993
orionmcc <orionmcc@gmail.com>
2020-05-01 07:08:29 -0400

    Moved locals.js into a local file in preparation for dockerizing. Added support for duplicating records in the editor.

--- scripts/testemail.mjs
+++ scripts/testemail.mjs
@@ -2,7 +2,7 @@
 'use strict';
 
 import EmailAdapter from '../lib/email';
-import { server as serverSettings } from "../local";
+import { server as serverSettings } from "../local/local";
 
 let EMAIL = new EmailAdapter();
 EMAIL.send("info@unpossiblegamelabs.com", { from: 'info@unpossibleworks.com', template: 'd-91855398cd1d4310bd5f4582b9b6d062', variables: { Server: serverSettings.host } });
