0900c5b1e247792f930fccdda9549789237f0355
orionmcc <orionmcc@gmail.com>
2022-12-27 22:11:14 -0500

    Test email fixed

--- scripts/testemail.mjs
+++ scripts/testemail.mjs
@@ -1,8 +1,10 @@
 
 'use strict';
 
-import EmailAdapter from '../lib/email.js';
-import { server as serverSettings } from "../local/local.js";
+import email from '../lib/email.js';
+import local from "../local/local.js";
+const serverSettings = local.server;
+const EmailAdapter = email.default;
 
 let EMAIL = new EmailAdapter();
 EMAIL.send("info@unpossiblegamelabs.com", { from: 'info@unpossibleworks.com', template: 'd-91855398cd1d4310bd5f4582b9b6d062', variables: { Server: serverSettings.host } });
