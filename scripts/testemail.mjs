fafc08243863fe1265cacfb799f0fe011adf981f
orionmcc <orionmcc@gmail.com>
2022-12-27 22:17:50 -0500

    Debug output

--- scripts/testemail.mjs
+++ scripts/testemail.mjs
@@ -4,7 +4,9 @@
 import email from '../lib/email.js';
 import local from "../local/local.js";
 const serverSettings = local.server;
+const emailSettings = local.email;
 const EmailAdapter = email.default;
 
 let EMAIL = new EmailAdapter();
+console.log('Sending with API key', emailSettings.APIKEY)
 EMAIL.send("info@unpossiblegamelabs.com", { from: 'info@unpossibleworks.com', template: 'd-91855398cd1d4310bd5f4582b9b6d062', variables: { Server: serverSettings.host } });
